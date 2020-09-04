983b12e911cbb29cf8c0e5ab7c75e0639b373493
dsmoby <dsmoby@gmail>
2020-09-04 19:59:15 +0200

    Challenge Day 2 complete -- hurrrrray

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Challenge 1 - Add borders
 
 function addBorder(arr) {
-  
   const arrNew = arr.map(el => {
     const modarr = `*${el}*`
     return modarr
@@ -9,21 +8,24 @@ function addBorder(arr) {
   const strLen = Math.max(...arrNew.map(d => d.length))
 
   console.log(strLen)
-  const repeatString = (n, str = "*") => (n >=1  ? str.repeat(n) : str)
+  const repeatString = (n, str = "*") => (n >= 1 ? str.repeat(n) : str)
   const border = repeatString(strLen)
   arrNew.unshift(border)
   arrNew.push(border)
   return arrNew
 }
 
-
-const inputArray = ["abc", "edfeg", "stuwyx"]
-
-console.log(addBorder(inputArray))
+// const inputArray = ["abc", "edfeg", "stuwyx"]
+// console.log(addBorder(inputArray))
 
 // Challenge Day 2 - Add the two digits of an integer
 
 function addTwoDigits(num) {
-  //  write code here.
-  const numStr = num.toString()
+  const [a, b] = num
+    .toString()
+    .split("")
+    .map(ds => parseInt(ds))
+  return a + b
 }
+
+console.log(addTwoDigits(29))
