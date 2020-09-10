409c5fc6d0c071b3bcd1ae3265be089bfcf34d33
dsmoby <dsmoby@gmail>
2020-09-10 02:39:27 +0200

    Day 4 complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Challenge 1 - Add borders
+/* / Challenge 1 - Add borders
 
 function addBorder(arr) {
   const arrNew = arr.map(el => {
@@ -22,12 +22,14 @@ function addBorder(arr) {
 
 // Challenge Day 2 - Add the two digits of an integer
 
+
 function addTwoDigits(num) {
   /*  const [a, b] = num
     .toString()
     .split("")
     .map(ds => parseInt(ds))
   return a + b */
+/*
   return num
     .toString()
     .split("")
@@ -35,6 +37,7 @@ function addTwoDigits(num) {
       return sum + parseInt(n)
     }, 0)
 }
+*/
 
 // return num.toString().split("").reduce((sum, n) => { return sum + parseInt(n) }, 0);
 
@@ -47,6 +50,7 @@ function addTwoDigits(num) {
 //--------------------------------------------------------------------------------------------
 
 // Day 3 - first duplicate
+/*
 function firstDuplicate(n) {
   const dup = []
   const notDup = []
@@ -63,23 +67,94 @@ function firstDuplicate(n) {
 const nums1 = [2, 1, 3, 5, 4, 6]
 const nums2 = [2, 1, 3, 5, 3, 2]
 
-const firstDuplicate2 = nums => nums.find((e, i, a) => a.indexOf(e) < i) || -1
+// const firstDuplicate2 = nums => nums.find((e, i, a) => a.indexOf(e) < i) || -1
 
-
-
-ٴٴٴٴ// console.log(firstDuplicate2(nums2))
-
+ٴٴٴٴ */ // console.log(firstDuplicate2(nums2))
 //--------------------------------------------------------------------------------------------
 
-// Day 4 - first duplicate
-
-function sumAllPrimes(n) {
+// Day 4 - Sum of prime numbers
+function sumAllPrimes(N) {
   let sum = 0
   const nums = []
-  for (let i=0; i<i; i++){
+  const isPrime = a => {
+    if (a === 2) {
+      sum += a
+      return true
+    }
+    if (a <= 1) {
+      return false
+    }
+    for (let j = 2; j < a; j++) {
+      if (a % j == 0) {
+        return false
+      }
+    }
+    sum += a
+    return true
+  }
+  for (let i = 0; i <= N; i++) {
     nums.push(i)
   }
 
-  return nums
+  const primes = nums.filter(n => isPrime(n) && n)
+  return sum
 }
 
+// other solutions
+
+function sumAllPrimes1(num) {
+  var sum = 0
+  for (let i = 2; i <= num; i++) {
+    var count = 2
+    var prime = true
+    while (i % count != 0 && count < i) {
+      count++
+      //console.log(`i: ${i}, count: ${count}`)
+    }
+    if (i != count) prime = false
+    else {
+      sum += i
+      console.log(`i: ${i}, sum: ${sum}`)
+    }
+  }
+  return sum
+}
+
+// solution 2
+const sumAllPrimes2 = num =>
+  ","
+    .repeat(num)
+    .slice(0, -1)
+    .split(",")
+    .map((e, i) => i + 1)
+    .filter(e => e > 1)
+    .filter(x =>
+      ","
+        .repeat(x)
+        .slice(0, -1)
+        .split(",")
+        .map((f, j) => j)
+        .filter(e => e > 1)
+        .every(e => x % e !== 0)
+    )
+    .reduce((a, b) => a + b, 0)
+
+// Solution 3
+function sumAllPrimes3(num) {
+  return [...Array(num - 1).keys()]
+    .map(data => data + 2)
+    .reduce((sum, test) => {
+      console.log(`${sum},  ${test}`)
+      if ([...Array(test - 2)].map((_, i) => i + 2).filter(i => test % i === 0).length == 0) {
+        return sum + test
+      }
+      return sum
+    })
+}
+
+const n = 977
+const res = sumAllPrimes(n)
+const res1 = sumAllPrimes1(n)
+
+console.log("sumAllPrimes: " + res)
+console.log("sumAllPrimes1: " + res1)
